63ffefc012e78a2ddf72d1d2a8b1efc4557e800f
lll <lll@gmail.com>
2025-12-17 19:58:15 +0530

    robo

--- adding.sh
+++ adding.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo "please enter the number"
-read NUMBER
-
-if [ $(( $NUMBER % 2)) -eq 0 ]; then
-echo "the given number is $NUMBER prime number"
-else 
-echo "the given number is $NUMBER not prime number"
-fi
+ echo "please enter the number" 
+ read NUMBER 
+ echo "result is  $((NUMBER * 2))"
