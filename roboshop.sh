224d5b35454e7b08196ea29c3a6f031a6721c8ff
lll <lll@gmail.com>
2025-12-17 17:26:25 +0530

    robo

--- roboshop.sh
+++ roboshop.sh
@@ -8,9 +8,10 @@ do
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
-    IP=$(aws ec2 run-instances --image-id  ami-09c813fb71547fc4f --instance-type t3.micro  --security-group-ids  sg-0f8421f2f12b0f03f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"  --query 'Instances[0].PrivateIPAddress' --output text)
-    else
-    IP=$(aws ec2 run-instances --image-id  ami-09c813fb71547fc4f --instance-type t3.micro  --security-group-ids  sg-0f8421f2f12b0f03f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"  --query 'Instances[0].PublicIPAddress' --output text)
-    fi
+        IP=$(aws ec2 describe-instances --instance-ids i-07f6e317caeff245d --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        else
+        IP=$(aws ec2 describe-instances --instance-ids i-07f6e317caeff245d --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        
+        
     echo "$instance: $IP"
     done
