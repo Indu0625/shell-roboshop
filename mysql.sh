c59e1669d9fbc5031e51dcb0a33c104570ab15d0
lll <lll@gmail.com>
2025-12-18 18:03:07 +0530

    robo

---
+++ mysql.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+ mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"|tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run this script with root previliges"
+    exit 1
+    fi
+    VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 .....$R failed $N"|tee -a $LOG_FILE
+    exit 1
+    else
+    echo -e  "$2 ....$G success $N"|tee -a $LOG_FILE
+    fi
+    }
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql server"
+systemctl enable mysqld
+VALIDATE $? "enable mysqld"
+systemctl start mysqld  
+VALIDATE $? "start mysqld"
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "setting up root password"
+
+ END_TIME=$(DATE +%S)
+    TOTAL_TIME=$( ($END_TIME-$START_TIME ))
+    echo -e "script executed in:$Y $TOTAL_TIME seconds $N"
