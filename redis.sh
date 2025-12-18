88c3d296a973afb0eeed02ac9eed109bde1187c5
lll <lll@gmail.com>
2025-12-18 17:48:37 +0530

    robo

---
+++ redis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+ mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"|tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run this script with root previliges"
+    exit 1
+    fi
+    VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 .....$R failed $N"|tee -a $LOG_FILE
+    exit 1
+    else
+    echo -e  "$2 ....$G success $N"|tee -a $LOG_FILE
+    fi
+    }
+    dnf module disable redis -y &>>$LOG_FILE
+    VALIDATE $? "Disabling default redis"
+
+dnf module enable redis:7 -y &>>LOG_FILE
+VALIDATE $? "Enabling default redis"
+dnf install redis -y &>>LOG_FILE
+VALIDATE $? "installing redis"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "allowing remote connection to redis"
+systemctl enable redis &>>LOG_FILE
+VALIDATE $? "enable redis"
+systemctl start redis &>>LOG_FILE
+VALIDATE $? "start redis"
