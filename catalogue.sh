5e34b2f6e75ce4b7649ae22f6c9161e064343a5e
lll <lll@gmail.com>
2025-12-18 15:44:32 +0530

    robo

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.daws86s.click
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
@@ -43,7 +44,7 @@ unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "unzip catalogue"
 npm install &>>$LOG_FILE
 VALIDATE $? "install dependices"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "copy systemctl services"
 systemctl daemon-reload
  
