2dd2f48d6b891f9da90cc835ebadc1ae4b0e5148
lll <lll@gmail.com>
2025-12-19 16:15:11 +0530

    robo

--- frontend.sh
+++ frontend.sh
@@ -26,24 +26,24 @@ VALIDATE(){
     echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-dnf module disable nginx -y &>>LOG_FILE
-dnf module enable nginx:1.24 -y &>>LOG_FILE
-dnf install nginx -y &>>LOG_FILE
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing ngnix"
 
-systemctl enable nginx &>>LOG_FILE
+systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx 
 VALIDATE $? "start nginx"
 rm -rf /usr/share/nginx/html/* 
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>LOG_FILE
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
 
 VALIDATE $? "downloading frontend"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/conf.d  &>>LOG_FILE
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf  &>>$LOG_FILE
 
 VALIDATE $? "copying ngnix"
 
