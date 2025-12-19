b8d214fc792df8ae09c3dfbf9ea8554de567dfcc
lll <lll@gmail.com>
2025-12-19 15:37:46 +0530

    robo

---
+++ frontend.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.click
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+echo "script started executed at: $(date)"| tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+echo "ERROR:: Please run this script with root previliges"
+exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+    else
+    echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+dnf module disable nginx -y &>>LOG_FILE
+dnf module enable nginx:1.24 -y &>>LOG_FILE
+dnf install nginx -y &>>LOG_FILE
+VALIDATE $? "Installing ngnix"
+
+systemctl enable nginx &>>LOG_FILE
+systemctl start nginx 
+VALIDATE $? "start nginx"
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>LOG_FILE
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>LOG_FILE
+
+VALIDATE $? "downloading frontend"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>LOG_FILE
+
+VALIDATE $? "copying ngnix"
+
+systemctl restart nginx 
+VALIDATE $? "restart nginx"
